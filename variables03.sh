9a305372f0dde07d5ebe8c51007c7a7d24ef6d48
telugukirankumar <telugukirankumar9121@gmail.com>
2026-04-02 10:14:18 +0530

    variables

--- variables03.sh
+++ variables03.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #DATE=$(date)
 START_TIME=$(date +%s)
-sleep 10 &
+sleep 10 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(END_TIME - START_TIME)
+TOTAL_TIME=$((END_TIME - START_TIME))
 echo "script execution time is $TOTAL_TIME seconds"

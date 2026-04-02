ddbb43157b626f75acd50cb53f94119f55e22038
telugukirankumar <telugukirankumar9121@gmail.com>
2026-04-02 10:13:31 +0530

    variables

--- variables03.sh
+++ variables03.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-DATE=$(date)
-echo "timestamp executed:$DATE"
+#DATE=$(date)
+START_TIME=$(date +%s)
+sleep 10 &
+END_TIME=$(date +%s)
+TOTAL_TIME=$(END_TIME - START_TIME)
+echo "script execution time is $TOTAL_TIME seconds"

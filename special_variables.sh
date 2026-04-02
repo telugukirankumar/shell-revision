7123d94e5993e6507aea0d4793879c2a6b36ae6d
telugukirankumar <telugukirankumar9121@gmail.com>
2026-04-02 10:30:55 +0530

    variables

---
+++ special_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
